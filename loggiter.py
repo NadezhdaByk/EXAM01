0e9649b4b40eb4abbece3b4206f60e46945dfc22
Nadezhda <nbykova79@gmail.com>
2023-02-18 09:25:29 +0300

    Add modul loggiter data

---
+++ loggiter.py
@@ -0,0 +1,11 @@
+import input_recipe as inprcp
+
+def log_recipe():
+    head=inprcp.input_heading()
+    
+    text=inprcp.input_recipe()
+    date=inprcp.input_date()    
+    with open('Recipes.csv', 'r+', encoding='UTF-8') as file:
+        all_data=file.read().split('\n')
+        id_recipe=len(all_data)-1
+        file.write(f'{id_recipe};{head};{text};{date}\n')
